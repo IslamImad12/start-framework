a2c47eeea7c160870f904fc9963a9ee97d1fdf27
IslamImad12 <smsm3559@gmail.com>
2024-02-11 03:16:49 +0200

    second  commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './Component/About.jsx';
 import Portfolio from './Component/Portfolio.jsx';
 import Contact from './Component/Contact.jsx';
 import Navbar from './Component/Navbar.jsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HashRouter, RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
 import Layout from './Component/Layout.jsx';
 import NotFound from './Component/NotFound.jsx';
 import style from './style.module.css'
@@ -13,16 +13,18 @@ import'bootstrap/dist/css/bootstrap.min.css';
 import'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-// import React, { Component } from 'react'
-// import About from './component/About.jsx';
-// import { Outlet } from 'react-router-dom'
-// import Counter from './component/Counter'
-// import Gallery from './component/Gallery.jsx';
-// import Pizza from './component/Pizza.jsx';
-// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-// import Layout from './component/Layout.jsx'
-// import Navbar from './component/Navbar.jsx';
 
+
+// let routers = createHashRouter([
+//   {path:'',element:<Layout></Layout> , children:[
+//     {index:true, element:<Home></Home>},
+//     {path:'home',element:<Home></Home>},
+//     {path:'about',element:<About></About>},
+//     {path:'portfolio',element:<Portfolio></Portfolio>},
+//     {path:'contact',element:<Contact></Contact>},
+//     {path:'*',element:<NotFound></NotFound>},
+//   ]},
+// ])
 let routers = createBrowserRouter([
   {path:'',element:<Layout></Layout> , children:[
     {index:true, element:<Home></Home>},
@@ -38,6 +40,7 @@ export default function App() {
   return (
     <>
     <RouterProvider router={routers} ></RouterProvider>
+    {/* <HashRouter router={routers} ></HashRouter> */}
     </>
     )
   }
